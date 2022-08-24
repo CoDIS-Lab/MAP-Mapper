refactoring, bug fixes, land masking and data visualisation

analysis/analysis.py
@@ -1,17 +1,18 @@
+
 import rasterio
 import numpy as np
-from patchify import patchify, unpatchify
-import math
 import os
-from paths import base_path
-data_path = os.path.join(base_path, "data", "historic_files")
-resolution = 100
+from utils.paths import base_path
+from pyproj import Transformer
 
-def get_files_for_density_calc():
+data_path = os.path.join(base_path, "data", "historic_files")
+
+
+def get_predictions():
     density_files = []
     for (root, dirs, files) in os.walk(data_path, topdown=True):
         for f in files:
-            if "unet_masked" in f:
+            if "prediction" in f:
                 file_path = os.path.join(root, f)
                 density_files.append(file_path)
     return density_files
@@ -24,79 +25,39 @@ def get_debris_coords(image_path):
     return debris_pixel_coords
 
 
-def get_debris_density(image_path):
+def get_plastic_points(image_path):
     src = rasterio.open(image_path)
     image = src.read(1)
     debris_pixels = np.argwhere(image == 1)
-    total_pixels = len(image) * len(image[0])
-    total_void_pixels = len(np.argwhere(image == 6))
-    density = len(debris_pixels) / (total_pixels - total_void_pixels)
-    return density
+    return debris_pixels
 
 
-def rounddown(x):
-    return int(math.floor(x / resolution)) * resolution
+all_point_data = []
 
-
-def get_plastic_density(image_path, resolution):
-    date = image_path.split("/")[-2]
-    tile = image_path.split("/")[-1].split("_")[0]
-    src = rasterio.open(image_path)
+for file in get_predictions():
+    plastic_pixels = get_plastic_points(file)
+    src = rasterio.open(file)
     image = src.read(1)
     meta = src.meta
+    geo_coords = [rasterio.transform.xy(meta['transform'], coord[0], coord[1], offset='center') for coord in plastic_pixels]
+    transformer = Transformer.from_crs(src.crs, "epsg:4326")
+    coords = [transformer.transform(coord[0], coord[1]) for coord in geo_coords]
+    all_point_data.extend(coords)
 
-    patches = patchify(image, (resolution, resolution), step=resolution) # split image into 100x100 small patches.
-    patches = patches.astype("float32")
-    for i in range(patches.shape[0]):
-        for j in range(patches.shape[1]):
-                patch = patches[i][j]
-                debris_pixels = np.argwhere(patch == 1)
-                total_pixels = len(patch) * len(patch[0])
-                total_void_pixels = len(np.argwhere(patch == 6))
-                if len(debris_pixels) > 0:
-                    density = len(debris_pixels) / (total_pixels - total_void_pixels)
-                elif total_pixels - total_void_pixels == 0:
-                    density = np.NaN
-                else:
-                    density = 0
-                density_patch = np.full(patch.shape, density)
-                patches[i, j, :, :] = density_patch
+import plotly.figure_factory as ff
 
-    shape = image.shape
-    shape = (rounddown(shape[0]), rounddown(shape[1]))
-    meta.update({"dtype": "float32", "height":shape[0], "width":shape[1]})
-    reconstructed_image = unpatchify(patches, shape)
-    density_file_path = os.path.join(base_path, 'data', 'density_calc')
-    density_image = os.path.join(density_file_path, tile + "_" + date + '_density.tif')
+import pandas as pd
+df = pd.DataFrame(all_point_data, columns =['centroid_lat', 'centroid_lon'])
+df.to_csv(os.path.join(data_path, "hond"))
+if not df.empty:
+    fig = ff.create_hexbin_mapbox(
+        data_frame=df, lat="centroid_lat", lon="centroid_lon",
+        nx_hexagon=15, opacity=0.6, labels={"color": "Point Count"},
+        show_original_data=True,
+        original_data_marker=dict(size=5, opacity=0.3, color="deeppink"),
+        color_continuous_scale="Thermal", min_count=0,
 
-    with rasterio.open(density_image, 'w', **meta) as dst:
-        dst.write(reconstructed_image.astype(rasterio.float32), 1)
+    )
+    fig.update_layout(mapbox_style="open-street-map", margin=dict(b=0, t=0, l=0, r=0))
 
-#
-# for file in get_files_for_density_calc():
-#     get_plastic_density(file, resolution)
-
-
-
-def get_longitudinal_plastic_density(dir_path):
-    path_list = []
-    stack = []
-    for (root, dirs, files) in os.walk(dir_path, topdown=True):
-        for f in files:
-            image_path = os.path.join(root, f)
-            path_list.append(image_path)
-    for path in path_list:
-        dataset = rasterio.open(path)
-        image = dataset.read(1)
-        stack.append(image)
-    stack = np.array(stack)
-    print(stack.shape)
-    longitudinal_density_map = np.nanmean(stack, axis=0)
-    # Read metadata of first file
-    with rasterio.open(path_list[0]) as src0:
-        meta = src0.meta
-
-    with rasterio.open("longitudinal_density_map.tif", 'w', **meta) as dst:
-        dst.write(longitudinal_density_map.astype(rasterio.float32), 1)
-
-get_longitudinal_plastic_density("/home/henry/PycharmProjects/plastic_pipeline/data/density_calc")
+    fig.show()

semantic_segmentation/debris_predictor.py
@@ -6,16 +6,15 @@ import rasterio
 import numpy as np
 from os.path import dirname as up
 import torch
-import torchvision.transforms as transforms
 from semantic_segmentation.Unet import UNet
-from paths import base_path
-from smooth_patches.smooth_tiled_predictions import predict_img_with_smooth_windowing
+from utils.paths import base_path
+from semantic_segmentation.smooth_patches.smooth_tiled_predictions import predict_img_with_smooth_windowing
+import torchvision.transforms as transforms
 
-bands_std = np.array([0.04725893, 0.04743808, 0.04699043, 0.04967381, 0.04946782, 0.06458357,
-                      0.07594915, 0.07120246, 0.08251058, 0.05111466, 0.03524419]).astype('float32')
+bands_mean = np.array(np.array([0.03163572, 0.03457443, 0.03436435, 0.02358126]).astype('float32'))
 
-bands_mean = np.array([0.05197577, 0.04783991, 0.04056812, 0.03163572, 0.02972606, 0.03457443,
-                       0.03875053, 0.03436435, 0.0392113, 0.02358126, 0.01588816]).astype('float32')
+bands_std = np.array([0.04967381, 0.06458357, 0.07120246, 0.05111466]).astype('float32')
+
 
 sys.path.append(os.path.join(up(up(up(os.path.abspath(__file__)))), 'utils'))
 
@@ -38,11 +37,10 @@ def predict(model, image):
 def predict_with_smooth_blending():
     print("making smoothly blended predictions....")
 
-    options = {"input_channels": 11,
-               "output_channels": 11,
-               "hidden_channels": 16,
+    options = {"input_channels": 4,
+               "output_channels": 2,
                "model_path": os.path.join(base_path, 'semantic_segmentation', 'unet', 'trained_models', 'model.pth'),
-               "gen_masks_path": os.path.join(base_path, 'data', 'predicted_unet')}
+               "gen_masks_path": os.path.join(base_path, 'data', 'predicted_patches')}
 
     transform_test = transforms.Compose([transforms.ToTensor()])
     standardization = transforms.Normalize(bands_mean, bands_std)
@@ -53,9 +51,8 @@ def predict_with_smooth_blending():
     else:
         device = torch.device("cpu")
 
-    model = UNet(input_bands=options['input_channels'],
-                 output_classes=options['output_channels'],
-                 hidden_channels=options['hidden_channels'])
+    model = UNet(in_channels=options['input_channels'],
+                 out_channels=options['output_channels'],)
 
     model.to(device)
 
@@ -67,7 +64,7 @@ def predict_with_smooth_blending():
         torch.cuda.empty_cache()
 
     model.eval()
-    patch_path = os.path.join(base_path, "data", "large_patches")
+    patch_path = os.path.join(base_path, "data", "patches")
     for file in os.listdir(patch_path):
         print("predictions on " + file)
         with rasterio.open(os.path.join(patch_path, file)) as src:
@@ -83,15 +80,18 @@ def predict_with_smooth_blending():
             with torch.no_grad():
                 predictions_smooth = predict_img_with_smooth_windowing(
                     input_img,
-                    window_size=256,
+                    window_size=32,
                     subdivisions=2,  # Minimal amount of overlap for windowing. Must be an even number.
-                    nb_classes=11,
+                    nb_classes=2,
                     model=model,
                     pred_func=lambda img_batch_subdiv: predict(model, torch.from_numpy(img_batch_subdiv))
                 )
                 # makes nan values max value in cloud channel
-                # nan values are classified as cloud (ignored in final analysis)
-                numpy.nan_to_num(predictions_smooth[:, :, 5], copy=False, nan=99)
+                # nan values are classified as cloud (ignored in final analysis) #not MD
+                numpy.nan_to_num(predictions_smooth[:, :, 1], copy=False, nan=99)
+                threshold = 0.995
+                MD = predictions_smooth[:, :, 0]
+                MD[MD < threshold] = 0
                 # uses the maximum value of the predictions channels to classify each pixel,
                 argmax = np.nanargmax(predictions_smooth, axis=2)
                 # increase each value of prediction mask by 1 (0 - 10 becomes 1 - 11)
@@ -106,6 +106,6 @@ def predict_with_smooth_blending():
                      "dtype": "uint8",
                      "nodata": 255})
                 with rasterio.open(
-                        os.path.join(base_path, "data", "predicted_unet", file.strip(".tif") + "_predict.tif"), "w",
+                        os.path.join(base_path, "data", "predicted_patches", file.strip(".tif") + "_predict.tif"), "w",
                         **out_meta) as dst:
                     dst.write(final_prediction)